9873af6923a6e1dac3dabca4465ce1d0c0d31234
root <root@Solomon-Cabin.localdomain>
2024-04-24 02:33:32 +0100

    cold fix

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        from models import storage  # Move the import here to avoid circular dependency
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -29,7 +33,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        from models import storage  # Move the import here to avoid circular dependency
         self.updated_at = datetime.now()
         storage.save()
 
@@ -42,3 +46,9 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        from models import storage  # Move the import here to avoid circular dependency
+        storage.delete(self)
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_objs = {}
+            for key, val in FileStorage.__objects.items():
+                if val.__class__ == cls:
+                    filtered_objs[key] = val
+            return filtered_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -49,6 +56,11 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-   def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
-        self.reload()
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
+            del self.__objects[key]
+
